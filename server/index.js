0cef0bec83617381d01623b8226594dc82324675
Rahul Ghimire <ghimirerahul554@gmail.com>
2023-07-15 16:49:09 +0545

    api added

--- server/index.js
+++ server/index.js
@@ -3,18 +3,22 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const app = express();
+
 //use express.json to get data in json format
 app.use(express.json());
 
 const PORT = process.env.PORT || 5500;
 
 //Connect to the mongodb
-
 mongoose
   .connect(process.env.DB_CONNECT)
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
 
+//Import the routes
+const TodoItemRoute = require("./routes/todoItems");
+
+app.use("/", TodoItemRoute);
 
 //Add PORT and connect to the server
 app.listen(PORT, () => console.log("Server connected"));
--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -3,4 +3,27 @@ const router = require("express").Router();
 //import todo Model
 const todoItemsModel = require("../models/todoItems");
 
-//create first route for the application
+//create first route to post data into the database
+router.post("/api/item", async (req, res) => {
+  try {
+    const newItem = new todoItemsModel({
+      item: req.body.item,
+    });
+    //save the item in the database
+    const saveItem = await newItem.save();
+    res.status(200).json("Item Added Successfully");
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//create second route to get data from the database
+router.get("/api/items", async (req, res) => {
+  try {
+
+  } catch (err) {
+    
+  }
+});
+
+module.exports = router;
