8929f917d1e72e0d162af35931981559af1562c2
Rahul Ghimire <ghimirerahul554@gmail.com>
2023-07-16 21:47:23 +0545

    frontend ui improved and added

--- client/todolist-mernapp/src/App.js
+++ client/todolist-mernapp/src/App.js
@@ -1,19 +1,43 @@
 import "./App.css";
 import { useState } from "react";
-
+import axios from "axios";
 function App() {
+  const [itemText, setItemText] = useState("");
+
+  const addItem = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5500/api/item", {
+        item: itemText,
+      });
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
       <header className="header">
         <h1>ToDo Application</h1>
       </header>
       <div className="container">
-        <form className="container_input">
-          <input type="text" placeholder="Add a Todo Item" />
-          <button type="submit">+</button>
+        <form className="container_input" onSubmit={(e) => addItem(e)}>
+          <input
+            type="text"
+            placeholder="Add a Todo Item"
+            onChange={(e) => setItemText(e.target.value)}
+            value={itemText}
+          />
+          <button type="submit">ADD +</button>
         </form>
       </div>
       <div className="container_todoItems">
+        <div className="todo-item">
+          <p className="item-content">This is a todo item</p>
+          <button className="btn update-btn">Update</button>
+          <button className="btn delete-btn">Delete</button>
+        </div>
         <div className="todo-item">
           <p className="item-content">This is a todo item</p>
           <button className="btn update-btn">Update</button>
