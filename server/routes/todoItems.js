e6f40b1ab36b37f916d63fb88e1d4f8d176d9b04
Rahul Ghimire <ghimirerahul554@gmail.com>
2023-07-16 08:42:45 +0545

    backend rest api completed

--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -20,9 +20,33 @@ router.post("/api/item", async (req, res) => {
 //create second route to get data from the database
 router.get("/api/items", async (req, res) => {
   try {
-
+    const allTodoItems = await todoItemsModel.find({});
+    res.status(200).json(allTodoItems);
   } catch (err) {
-    
+    res.json(err);
+  }
+});
+
+//Update the item
+router.put("/api/item/:id", async (req, res) => {
+  try {
+    //find item by id and update it
+    let updateItem = await todoItemsModel.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json("Item updated successfully");
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//Delete item from a database
+router.delete("/api/item/:id", async (req, res) => {
+  try {
+    const deleteItem = await todoItemsModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Item deleted successfully");
+  } catch (err) {
+    res.json(err);
   }
 });
 
